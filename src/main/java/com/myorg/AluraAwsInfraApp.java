a342d7ae43d40c09366ea41ddf28c04a1c69b1f9
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2024-07-01 20:30:43 -0300

    feat: creating vpc

--- src/main/java/com/myorg/AluraAwsInfraApp.java
+++ src/main/java/com/myorg/AluraAwsInfraApp.java
@@ -10,33 +10,15 @@ public class AluraAwsInfraApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new AluraAwsInfraStack(app, "AluraAwsInfraStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
-
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
-                */
-
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
-
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
+        final String vpcStackId = "Vpc";
+        new AluraVpcStack(app, vpcStackId);
 
         app.synth();
     }
 }
 
+/*
+fargate - menos trabalho que o ec2 para gerenciar.
+O ECS (Elastic Container Service) é uma solução da AWS que tem por objetivo facilitar o gerenciamento e disponibilização de aplicações conteinerizadas na nuvem. Esse serviço é composto por alguns recursos, como Container definition, Task definition, Service, Cluster e VPC.
+
+ */
