75d122d116b52783c58842501096c53beffd372f
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2024-07-02 13:12:38 -0300

    feat: enabling auto scaling

--- src/main/java/com/myorg/AluraServiceStack.java
+++ src/main/java/com/myorg/AluraServiceStack.java
@@ -1,15 +1,10 @@
 package com.myorg;
 
-import software.amazon.awscdk.Fn;
-import software.amazon.awscdk.RemovalPolicy;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
 import software.amazon.awscdk.services.ecr.IRepository;
 import software.amazon.awscdk.services.ecr.Repository;
-import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
-import software.amazon.awscdk.services.ecs.Cluster;
-import software.amazon.awscdk.services.ecs.ContainerImage;
-import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
 import software.amazon.awscdk.services.logs.LogGroup;
@@ -27,11 +22,11 @@ public class AluraServiceStack extends Stack {
         super(scope, id, props);
 
         // Create a load-balanced Fargate service and make it public
-        ApplicationLoadBalancedFargateService.Builder.create(this, "AluraFargateService")
+        ApplicationLoadBalancedFargateService service = ApplicationLoadBalancedFargateService.Builder.create(this, "AluraFargateService")
                 .serviceName("alura-service-ola")
                 .cluster(cluster)           // Required
                 .cpu(512)                   // Default is 256
-                .desiredCount(3)            // Default is 1 / number of instances.
+                .desiredCount(1)            // Default is 1 / number of instances.
                 .listenerPort(8080)
                 .assignPublicIp(true)
                 .taskImageOptions(
@@ -45,6 +40,8 @@ public class AluraServiceStack extends Stack {
                 .memoryLimitMiB(1024)       // Default is 512
                 .publicLoadBalancer(true)   // Default is false
                 .build();
+
+        enableAutoScaling(service);
     }
 
     private Map<String, String> getAppEnv() {
@@ -66,6 +63,25 @@ public class AluraServiceStack extends Stack {
                         .removalPolicy(RemovalPolicy.DESTROY) // se apagar a stack, apaga os logs
                         .build())
                 .streamPrefix("PedidosMS")
+                .build());
+    }
+
+    private void enableAutoScaling(ApplicationLoadBalancedFargateService service) {
+        ScalableTaskCount scalableTarget = service.getService().autoScaleTaskCount(EnableScalingProps.builder()
+                .minCapacity(1) // min e max de instancias
+                .maxCapacity(20)
+                .build());
+
+        scalableTarget.scaleOnCpuUtilization("CpuScaling", CpuUtilizationScalingProps.builder()
+                .targetUtilizationPercent(70)
+                .scaleInCooldown(Duration.seconds(120)) // quanto tempo com 70% para subir a instancia
+                .scaleOutCooldown(Duration.seconds(60)) // para descer a instancia
+                .build());
+
+        scalableTarget.scaleOnMemoryUtilization("MemoryScaling", MemoryUtilizationScalingProps.builder()
+                .targetUtilizationPercent(70)
+                .scaleInCooldown(Duration.seconds(60))
+                .scaleOutCooldown(Duration.seconds(60))
                 .build());
     }
 }
