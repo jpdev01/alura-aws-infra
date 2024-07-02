62f9effafa7543c3b6972ff7845ec139d14193ec
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2024-07-02 09:16:35 -0300

    upd: configurando app

--- src/main/java/com/myorg/AluraServiceStack.java
+++ src/main/java/com/myorg/AluraServiceStack.java
@@ -1,5 +1,6 @@
 package com.myorg;
 
+import software.amazon.awscdk.Fn;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ecs.Cluster;
@@ -7,6 +8,8 @@ import software.amazon.awscdk.services.ecs.ContainerImage;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
 import software.constructs.Construct;
+
+import java.util.Map;
 
 public class AluraServiceStack extends Stack {
 
@@ -30,9 +33,18 @@ public class AluraServiceStack extends Stack {
                                 .image(ContainerImage.fromRegistry("jacquelineoliveira/ola1.0"))
                                 .containerPort(8080)
                                 .containerName("app_ola")
+                                .environment(getAppEnv())
                                 .build())
                 .memoryLimitMiB(1024)       // Default is 512
                 .publicLoadBalancer(true)   // Default is false
                 .build();
+    }
+
+    private Map<String, String> getAppEnv() {
+        return Map.of(
+                "SPRING_DATASOURCE_URL", "jdbc:mysql://" + Fn.importValue("pedidos-db-endpoint") + ":3306/pedidos_ms?createDatabaseIfNotExist=true",
+                "SPRING_DATASOURCE_USERNAME", "admin",
+                "SPRING_DATASOURCE_PASSWORD", Fn.importValue("pedidos-db-password")
+        );
     }
 }
