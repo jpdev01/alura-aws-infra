63951806c3b25960a36a60d2d9c826f20522665d
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2024-07-02 09:17:33 -0300

    upd: rds precisa subir antes do service

--- src/main/java/com/myorg/AluraAwsInfraApp.java
+++ src/main/java/com/myorg/AluraAwsInfraApp.java
@@ -16,6 +16,7 @@ public class AluraAwsInfraApp {
 
         AluraServiceStack aluraServiceStack = new AluraServiceStack(app, "Service", clusterStack.getCluster());
         aluraServiceStack.addDependency(clusterStack);
+        aluraServiceStack.addDependency(rdsStack);
         app.synth();
     }
 }
